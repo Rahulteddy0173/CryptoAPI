8f7f19cf2b2ea81d11c399be5e051694ed6defb4
rahul <rahulteddy0173@gmail.com>
2023-06-02 16:34:31 +0530

    Crypto API

---
+++ script.js
@@ -0,0 +1,13 @@
+// function getApiData() {
+//     fetch ('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+//     .then(Response => Response.json())
+//     .then ((Response) => {
+//         console.log(getApiData)
+//     })
+
+// }
+
+
+// function searchInputValue(searchValue){
+    
+// }
